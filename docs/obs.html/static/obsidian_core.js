d239c67b85a1d4a921ebb1f9da4d8bf4d43c7a65
iatenothingbutriceforthreedays <darwinvickers@gmail.com>
2024-03-21 10:17:08 +0800

    add a note to cv about inspirations

--- docs/obs.html/static/obsidian_core.js
+++ docs/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 0;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '98098067384466613085922690611163605307'                       // used to check whether the localStorage data is stale
+var gzip_hash = '304721520840311059853837584848981404966'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
